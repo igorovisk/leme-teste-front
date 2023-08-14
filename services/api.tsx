2c104fa05708afbe4df720a4a709501fc715fda4
Igor Augusto Corso Borgio <igorovisk@gmail.com>
2023-08-14 03:15:19 -0300

    Fix fields

--- services/api.tsx
+++ services/api.tsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-   // baseURL: "https://leme-teste-api.vercel.app/",
-   baseURL: "http://localhost:3005",
+   baseURL: "https://leme-teste-api.vercel.app/",
+   // baseURL: "http://localhost:3005",
    timeout: 10000,
 });
 export default api;
